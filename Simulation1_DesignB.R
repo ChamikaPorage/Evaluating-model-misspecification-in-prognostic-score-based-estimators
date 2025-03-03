ef0b19019696abeab18a8c6d4531cc639f01380f
ChamikaPorage <92798615+ChamikaPorage@users.noreply.github.com>
2025-03-03 13:17:14 +0100

    Update and rename Sim1_DesB_cf.R to Simulation1_DesignB.R

--- Simulation1_DesignB.R
+++ Simulation1_DesignB.R
@@ -1,6 +1,6 @@
-##########
-#DESIGN B#
-##########
+#######################
+#Simulation1: Design B#
+#######################
 
 #contains function with argument N = sample size and seed
 # that generates 1000 datasets of Design B,  simulation 1.
