799fe8e6c439bb67b704b22c91ce6dbacbc444ee
ChamikaPorage <92798615+ChamikaPorage@users.noreply.github.com>
2026-03-20 15:06:19 +0100

    Update Simulation1_DesignA.R

--- Simulation1_DesignA.R
+++ Simulation1_DesignA.R
@@ -6,12 +6,11 @@
 
 
 library(ranger)
-library(e1071)
 library(caret)
 
 gen.data.A <- function(N, seed){
   set.seed(seed)
-  datat2 <- list(0)
+  datat2 <- vector("list", 1000)
   for (i in 1:1000) 
   {
     
@@ -34,51 +33,43 @@ gen.data.A <- function(N, seed){
     d1 <- -0.15
     d2<- 0.4
     
-    prob <- ((1 + exp(a + b * x1 + b2 * x12 + c * x2 + c2 * x22 + d1 *e1 + d2 * e2 )))^ (-1)
-    tr <- rbinom(N, 1, prob)
+    eta  <- -(a + b*x1 + b2*x12 + c*x2 + c2*x22 + d1*e1 + d2*e2)
+    prob <- plogis(eta)   
+    tr   <- rbinom(N, 1, prob)
     
     eps0 <- rnorm(N, 0, 1)
     eps1 <- rnorm(N, 0, 1)
     
-    y0 <- 20 - 5 * x1 + 2 * x12 - 5 * x2 + 2 * x22 + 4 * x_12 + e1*tr + e2*tr + eps0
-    y1 <- 22 - 5 * x1 + 2 * x12 - 5 * x2 + 2 * x22 + 4 * x_12 + e1*tr + e2*tr + eps1
+    y0 <- 20 - 5 * x1 + 2 * x12 - 5 * x2 + 2 * x22 + 4 * x_12 + eps0
+    y1 <- 22 - 5 * x1 + 2 * x12 - 5 * x2 + 2 * x22 + 4 * x_12 + e1 + e2 + eps1
     y  <- ifelse(tr == 1, y1, y0)
     
     datat <- as.data.frame(cbind(x1, x12, x2, x22, x_12, tr, e1, e2, y0, y1, y))
     colnames(datat) <- c("x1", "x12", "x2", "x22", "x_12", "tr","e1","e2", "y0", "y1", "y")
     
-    #PS MODELS
-    mod.ps <- glm(tr ~ x1 + x12 + x2 + x22 + x_12 + e1 + e2, family = binomial, data = datat)
-    ps <- fitted.values(mod.ps, type = "response") 
-    datat$ps <- ps
-    
-    mod.psf1 <- glm(tr ~ x1 + x2 + x22 + x_12 + e1+ e2 , family = binomial(link = "logit"), data = datat)
-    psf1 <- fitted.values(mod.psf1, type = "response") 
-    datat$psf1 <- psf1
-    
     #OR MODELS
     
-    #linear models
+    ##linear models##
     
-    datat0 <- subset(datat, datat$tr == 0)
-    datat1 <- subset(datat, datat$tr == 1)
+    datat0 <- subset(datat, tr == 0)
+    datat1 <- subset(datat, tr == 1)
     
     #TRUE OR
     mod0 <- lm(y ~ x1 + x12 + x2 + x22 + x_12 + e1 + e2, data = datat0)
-    mu0 <- predict(mod0, newdata = datat, type = "response")
+    mu0 <- predict(mod0, newdata = datat)
     
     mod1 <- lm(y ~ x1 + x12 + x2 + x22 + x_12 + e1 + e2, data = datat1)
-    mu1 <- predict(mod1, newdata = datat, type = "response")
+    mu1 <- predict(mod1, newdata = datat)
     
     #FALSE OR
     
     mod0f <- lm(y ~ x1 + x2 + x22 + x_12 + e1 + e2, data = datat0)
-    mu0f <- predict(mod0f, newdata = datat, type = "response")
+    mu0f <- predict(mod0f, newdata = datat)
     
     mod1f <- lm(y ~ x1 + x2 + x22 + x_12 + e1 + e2, data = datat1)
-    mu1f <- predict(mod1f, newdata = datat, type = "response")
+    mu1f <- predict(mod1f, newdata = datat)
     
-    ###Non-linear model
+    ##Non-linear model##
     
     # Cross-fitting setup
     folds <- createFolds(datat$y, k = 5, list = TRUE)
@@ -94,6 +85,8 @@ gen.data.A <- function(N, seed){
       # Separate treated and untreated data
       data0_train <- subset(train_data, tr == 0)
       data1_train <- subset(train_data, tr == 1)
+
+      #True non-linear model
       
       # Train random forest on untreated (Tr == 0)
       model_rf_0 <- ranger(y ~ x1 + x12 + x2 + x22 + x_12 + e1 + e2, data = data0_train, num.trees = 300, mtry = 2, min.node.size = 5)
@@ -104,6 +97,7 @@ gen.data.A <- function(N, seed){
       murf1[valid_idx] <- predict(model_rf_1, data = valid_data[, c("x1","x12", "x2", "x22", "x_12", "e1", "e2")])$predictions
       
       #Fasle non-linear model
+      
       # Train random forest on untreated (Tr == 0)
       mod_rf_0 <- ranger(y ~ x1  + x2 + x22 + x_12 + e1 + e2, data = data0_train, num.trees = 300, mtry = 2, min.node.size = 5)
       murf0_f[valid_idx] <- predict(mod_rf_0, data = valid_data[, c("x1", "x2", "x22", "x_12", "e1", "e2")])$predictions
@@ -114,18 +108,9 @@ gen.data.A <- function(N, seed){
       
     }
     
-     mu02 <- mu0^2
-     mu12 <- mu1^2
-     mu01 <- mu0*mu1
-     mu0f2 <- mu0f^2
-     mu1f2 <- mu1f^2
-     muf01 <- mu0f*mu1f
-     murf02 <- murf0^2
-     murf12 <- murf1^2
-     murf10 <- murf0*murf1
-    
-    datat2[[i]] <- as.data.frame(cbind(tr, y, ps, psf1, mu0, mu1, mu0f, mu1f, mu02, mu12, mu01, mu0f2, mu1f2, muf01, murf0, murf1,  murf0_f, murf1_f, murf02, murf12, murf10))
-    colnames(datat2[[i]]) <- c("tr", "y" ,"ps", "psf1" ,"mu0", "mu1", "mu0f", "mu1f","mu02", "mu12", "mu01", "mu0f2", "mu1f2", "muf01", "murf0", "murf1", "murf0_f", "murf1_f", "murf02", "murf12", "murf10")
+    datat2[[i]] <- data.frame(tr, y, mu0, mu1, mu0f, mu1f, murf0, murf1, murf0_f, murf1_f)
+    colnames(datat2[[i]]) <- c("tr", "y", "mu0", "mu1", "mu0f", "mu1f", "murf0", "murf1", "murf0_f", "murf1_f")
   }
   return(datat2)
 }
+
